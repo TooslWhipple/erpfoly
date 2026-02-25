b62f972003aba067f536004c388907f518867bb9
Carlos Ríos <carlos@dicalzeus.com>
2026-02-24 18:05:39 -0700

    [ADD] Proveedores de reparaciones: listado con API, MultiSelectAutocomplete, eliminar (soft delete), buscador con lupa y label arriba

---
+++ src/components/MultiSelectAutocomplete/index.ts
@@ -0,0 +1,2 @@
+export { MultiSelectAutocomplete } from "./MultiSelectAutocomplete";
+export type { MultiSelectAutocompleteProps, SelectableItem } from "./MultiSelectAutocomplete";
--- src/components/index.ts
+++ src/components/index.ts
@@ -22,6 +22,8 @@ export { ModalForm } from "./ModalForm";
 export type { ModalFormProps } from "./ModalForm";
 export { MultiSelectChips } from "./MultiSelectChips";
 export type { MultiSelectChipsProps, SelectableItem } from "./MultiSelectChips";
+export { MultiSelectAutocomplete } from "./MultiSelectAutocomplete";
+export type { MultiSelectAutocompleteProps, SelectableItem as SelectableItemAutocomplete } from "./MultiSelectAutocomplete";
 export { Tabs } from "./Tabs";
 export type { TabItem, TabsProps } from "./Tabs";
 export { OrderCard, OrderList } from "./OrderCard";
